48cc5dbc7c3928c60a803ffeb763e2046c33f9b6
unknown <mosha3t@gmail.com>
2024-03-17 11:09:41 +0200

    added routing

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,39 +1,53 @@
-import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import { HeaderBasket } from "../../eCommerce";
-import styles from "./styles.module.css";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 
-const {headerContainer,headerLogo} = styles
-function Header() {
+import styles from "./styles.module.css";
+const { headerContainer, headerLogo } = styles;
+
+const Header = () => {
   return (
     <header>
       <div className={headerContainer}>
         <h1 className={headerLogo}>
           <span>our</span> <Badge bg="info">Ecom</Badge>
         </h1>
-        <HeaderBasket/>
+
+        <HeaderBasket />
       </div>
-      <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        bg="dark"
+        data-bs-theme="dark"
+      >
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Categories</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-
+              <Nav.Link as={NavLink} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="categories">
+                Categories
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="about">
+                About
+              </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#home">Login</Nav.Link>
-              <Nav.Link href="#link">Register</Nav.Link>
-      
-
+              <Nav.Link as={NavLink} to="login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="register">
+                Register
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
   );
-}
+};
 
 export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import {MainLayout} from '@layouts/index'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import AppRouter from '@routes/AppRouter';
+import '@styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <MainLayout/>
+    <AppRouter/>
 )
