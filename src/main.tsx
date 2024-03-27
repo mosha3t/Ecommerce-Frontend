7299571437e2da57c6062839f6df64c12679e31b
unknown <mosha3t@gmail.com>
2024-03-27 10:02:44 +0200

    added categories slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppRouter from '@routes/AppRouter';
-import '@styles/global.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AppRouter/>
-)
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from "@routes/AppRouter";
+import "@styles/global.css";
+import { Provider } from "react-redux";
+import store from "@store/index";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
